c234598eaf7b703478051275b401a68f876886bb
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2017-04-05 10:47:30 +0200

    added new files

---
+++ examples/LLP/join/basic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATADIR=$DIR/../../../tests/data
+
+# default 
+echo "# Input: small table join"
+LLP join --join accession=accession --how left $DATADIR/accessions_col3.txt $DATADIR/nucl_wg.acc2taxid
+
+echo "# Input: small table join with gzip file"
+LLP join --join accession=accession --how left $DATADIR/accessions_col3.gz $DATADIR/nucl_wg.acc2taxid
+
+echo "# Input: same table; inner join on 2 columns"
+LLP join --join accession=accession,OTU=OTU --how left $DATADIR/accessions_col3.txt $DATADIR/accessions_col3.txt
